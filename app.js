916ed91b59fc533089df75de7f82aa256b2459ab
rlauderdale <rjlauderdale26@gmail.com>
2019-04-06 22:09:13 -0500

    local storage
    setup local storage function for new location data

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
+// Init Storage
+const storage = new Storage();
+// Get stored location data
+const weatherLocation = storage.getLocationData();
 // Init weather object
-const weather = new Weather('Chicago', 'us');
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
 // Init UI
 const ui = new UI();
 
 // Get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
+
+// Change location event
+document.getElementById('w-change-btn').addEventListener('click', 
+(e) => {
+  const city = document.getElementById('city').value;
+  const state = document.getElementById('state').value;
+
+  // Change Location
+  weather.changeLocation(city, state);
+
+  // Set location in LocalStorage
+  storage.setLocationData(city, state);
+
+  getWeather();
+  $('#locModal').modal('hide');
+});
 
 
 function getWeather(){
